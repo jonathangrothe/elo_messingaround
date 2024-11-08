7007e1647cb7a7f956f2aa6267d79b028e65d4f2
Jonathan Grothe <grothej@whitman.edu>
2024-11-08 14:21:51 -0800

    generalizes it to a number of riders

--- elo_cycling_sprinters.R
+++ elo_cycling_sprinters.R
@@ -8,35 +8,20 @@ headtohead <- read_csv("headtohead.csv")
 
 #step 2: do the elo process
 
-
-#a function which calculates the expected score of a rider based on elo
-
-# rider_vector <- function(df){
-#   return (unlist(df[1]))
-# }
-
-
 num_riders <- function(rider_vector){
   return (length(rider_vector))
 }
 
- 
-# elo_vector <- function(df){
-#   return (unlist(df[ncol(df)]))
-# }
-
-# stage_vector <- function(df,stage){
-#   return (unlist(df[stage+1]))
-# }
-
+#finds the expected score of a rider based on their
 ex_score <- function(elo_vector, rider){
   sum <- 0 
   rider_elo <- elo_vector[rider]
-  for (i in 1:14){
+  num_riders <- num_riders(elo_vector)
+  for (i in 1:num_riders){
     h2hscore <- 1/(1+10^((elo_vector[i]-rider_elo)/400))
     sum = sum + h2hscore
   }
-  ex_score <- sum/91
+  ex_score <- (2*sum)/(num_riders*(num_riders-1))
   return(ex_score)
 }
 
@@ -50,17 +35,18 @@ for (i in 1:14){
 update <- function(scoring_vector, elo_vector, stage_vector, rider){
   actual_result <- scoring_vector[stage_vector[rider]]
   expected_result <- ex_score(elo_vector, rider)
-  updated_elo = elo_vector[rider] + 20*13*(actual_result-expected_result)
+  updated_elo = elo_vector[rider] + 20*(num_riders(elo_vector)-1)*(actual_result-expected_result)
   return (updated_elo)
 }
 
 elo <- function(df, scoring_vector){
   rider_vector <- unlist(df[1])
   elo_vector <- unlist(df[ncol(df)])
+  num_riders <- num_riders(elo_vector)
   for (x in 2:(ncol(df)-1)){
-    updated_elo <- vector(length=14)
+    updated_elo <- vector(length=num_riders)
     stage <- unlist(df[x])
-    for (i in 1:14){
+    for (i in 1:num_riders){
       rider_elo <- update(scoring_vector, elo_vector, stage, i)
       updated_elo[i] <- rider_elo
     }
